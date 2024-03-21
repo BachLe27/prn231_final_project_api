7d2c3f1903db3fa9233dde5a273c85a6b00e3a22
BachLe27 <bachlamsung123@gmail.com>
2024-03-21 20:43:18 +0700

    Update controllers

---
+++ api/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly project_prn231Context _context;
+
+        public StudentController(project_prn231Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var students = _context.Users.Where(x => x.Role.Equals("student")).ToList();
+            return Ok(students);
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+    }
+}
